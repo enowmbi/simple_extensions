7a8b3d46b220dc286d5e88abbb3976ff7328f70b
Enow Mbi <benowmbi@yahoo.com>
2020-05-09 08:59:42 +0100

    Add require statements for each extension

--- lib/extension.rb
+++ lib/extension.rb
@@ -3,4 +3,7 @@ require "extension/version"
 module Extension
   class Error < StandardError; end
   # Your code goes here...
+  require "extension/string_extensions"
+  require "extension/integer_extensions"
+  require "extension/array_extensions"
 end
